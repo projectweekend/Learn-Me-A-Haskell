682732c8207c1ce0a76e160e381fa1d07db551ba
Brian Hines <brian@projectweekend.net>
2015-01-06 20:59:06 -0600

    More function type stuff

--- chapter-02/types.hs
+++ chapter-02/types.hs
@@ -4,3 +4,11 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
